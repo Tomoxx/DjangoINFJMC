72c2fa36bdb996345922ed3853b12e6bcf5b959e
Tomoxx <tomi-super@live.cl>
2022-10-18 00:00:25 -0300

    Version 18-10

--- INFJMC/core/views.py
+++ INFJMC/core/views.py
@@ -5,12 +5,14 @@ from django.http import HttpResponse
 def home(request):
     # html = "<h1>Informática USM</h1> <a href='{% url 'carreras' %}'>Carreras</a>"
     # return HttpResponse(html)
-    return render(request,'core/swag.html')
+    return render(request,'core/home.html')
 
 def carreras(request):
-    html = "<h1>Carreras</h1>"
-    return HttpResponse(html)
+    # html = "<h1>Carreras</h1>"
+    # return HttpResponse(html)
+    return render(request,'core/carreras.html')
 
 def docentes(request):
-    html = "<h1>Docentes</h1>"
-    return HttpResponse(html)
+    # html = "<h1>Docentes</h1>"
+    # return HttpResponse(html)
+    return render(request,'core/docentes.html')
